3315411ddb09206e3ff55289f3b774ef62bb4a4c
charulata-09 <charulatta09@gmail.com>
2026-07-22 10:15:32 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,2 @@
-export { createAdaptorServer, serve } from './server.js';
-export { getRequestListener } from './listener.js';
-export { RequestError } from './request.js';
-export { Http2Bindings, HttpBindings, ServerType } from './types.js';
-import 'node:net';
-import 'node:http';
-import 'node:http2';
-import 'node:https';
+export * from "./node-hfs.js";
+export { Hfs } from "@humanfs/core";

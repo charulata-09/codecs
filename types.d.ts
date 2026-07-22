ff72205d52e77079501c8e391c7bd5e4457eb708
charulata-09 <charulatta09@gmail.com>
2026-07-22 10:20:22 +0530

    Add files via upload

--- types.d.ts
+++ types.d.ts
@@ -1,44 +1,43 @@
-import { IncomingMessage, ServerResponse, Server, ServerOptions as ServerOptions$1, createServer } from 'node:http';
-import { Http2ServerRequest, Http2ServerResponse, Http2Server, Http2SecureServer, ServerOptions as ServerOptions$3, createServer as createServer$2, SecureServerOptions, createSecureServer } from 'node:http2';
-import { ServerOptions as ServerOptions$2, createServer as createServer$1 } from 'node:https';
-
-type HttpBindings = {
-    incoming: IncomingMessage;
-    outgoing: ServerResponse;
+import type { SourceMapSegment } from './sourcemap-segment';
+export interface SourceMapV3 {
+    file?: string | null;
+    names: readonly string[];
+    sourceRoot?: string;
+    sources: readonly (string | null)[];
+    sourcesContent?: readonly (string | null)[];
+    version: 3;
+    ignoreList?: readonly number[];
+}
+export interface EncodedSourceMap extends SourceMapV3 {
+    mappings: string;
+}
+export interface DecodedSourceMap extends SourceMapV3 {
+    mappings: readonly SourceMapSegment[][];
+}
+export interface Pos {
+    line: number;
+    column: number;
+}
+export interface OriginalPos extends Pos {
+    source: string;
+}
+export interface BindingExpressionRange {
+    start: Pos;
+    expression: string;
+}
+export type Mapping = {
+    generated: Pos;
+    source: undefined;
+    original: undefined;
+    name: undefined;
+} | {
+    generated: Pos;
+    source: string;
+    original: Pos;
+    name: string;
+} | {
+    generated: Pos;
+    source: string;
+    original: Pos;
+    name: undefined;
 };
-type Http2Bindings = {
-    incoming: Http2ServerRequest;
-    outgoing: Http2ServerResponse;
-};
-type FetchCallback = (request: Request, env: HttpBindings | Http2Bindings) => Promise<unknown> | unknown;
-type NextHandlerOption = {
-    fetch: FetchCallback;
-};
-type ServerType = Server | Http2Server | Http2SecureServer;
-type createHttpOptions = {
-    serverOptions?: ServerOptions$1;
-    createServer?: typeof createServer;
-};
-type createHttpsOptions = {
-    serverOptions?: ServerOptions$2;
-    createServer?: typeof createServer$1;
-};
-type createHttp2Options = {
-    serverOptions?: ServerOptions$3;
-    createServer?: typeof createServer$2;
-};
-type createSecureHttp2Options = {
-    serverOptions?: SecureServerOptions;
-    createServer?: typeof createSecureServer;
-};
-type ServerOptions = createHttpOptions | createHttpsOptions | createHttp2Options | createSecureHttp2Options;
-type Options = {
-    fetch: FetchCallback;
-    overrideGlobalObjects?: boolean;
-    autoCleanupIncoming?: boolean;
-    port?: number;
-    hostname?: string;
-} & ServerOptions;
-type CustomErrorHandler = (err: unknown) => void | Response | Promise<void | Response>;
-
-export { CustomErrorHandler, FetchCallback, Http2Bindings, HttpBindings, NextHandlerOption, Options, ServerOptions, ServerType };

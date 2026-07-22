242f4ad6592d2b3a800635b5c458fc0059f71981
charulata-09 <charulatta09@gmail.com>
2026-07-22 10:22:30 +0530

    Add files via upload

--- types.ts
+++ types.ts
@@ -1,61 +1,27 @@
-// import type { GeneratedRange, OriginalScope } from '@jridgewell/sourcemap-codec';
-import type { SourceMapSegment } from './sourcemap-segment';
+import type { SourceMapInput } from '@jridgewell/trace-mapping';
 
-export interface SourceMapV3 {
-  file?: string | null;
-  names: readonly string[];
-  sourceRoot?: string;
-  sources: readonly (string | null)[];
-  sourcesContent?: readonly (string | null)[];
-  version: 3;
-  ignoreList?: readonly number[];
-}
+export type {
+  SourceMapSegment,
+  DecodedSourceMap,
+  EncodedSourceMap,
+} from '@jridgewell/trace-mapping';
 
-export interface EncodedSourceMap extends SourceMapV3 {
-  mappings: string;
-  // originalScopes: string[];
-  // generatedRanges: string;
-}
+export type { SourceMapInput };
 
-export interface DecodedSourceMap extends SourceMapV3 {
-  mappings: readonly SourceMapSegment[][];
-  // originalScopes: readonly OriginalScope[][];
-  // generatedRanges: readonly GeneratedRange[];
-}
-
-export interface Pos {
-  line: number; // 1-based
-  column: number; // 0-based
-}
-
-export interface OriginalPos extends Pos {
+export type LoaderContext = {
+  readonly importer: string;
+  readonly depth: number;
   source: string;
-}
+  content: string | null | undefined;
+  ignore: boolean | undefined;
+};
 
-export interface BindingExpressionRange {
-  start: Pos;
-  expression: string;
-}
+export type SourceMapLoader = (
+  file: string,
+  ctx: LoaderContext,
+) => SourceMapInput | null | undefined | void;
 
-// export type OriginalScopeInfo = [number, number, string[] | undefined];
-// export type GeneratedRangeInfo = [GeneratedRange, string[] | undefined];
-
-export type Mapping =
-  | {
-      generated: Pos;
-      source: undefined;
-      original: undefined;
-      name: undefined;
-    }
-  | {
-      generated: Pos;
-      source: string;
-      original: Pos;
-      name: string;
-    }
-  | {
-      generated: Pos;
-      source: string;
-      original: Pos;
-      name: undefined;
-    };
+export type Options = {
+  excludeContent?: boolean;
+  decodedMappings?: boolean;
+};
